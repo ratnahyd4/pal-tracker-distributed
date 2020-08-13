4823f91d50d082faa66fac2f16d5bffaecd0611c
ratnahyd4 <ratna.hyd4@gmail.com>
2020-08-13 14:15:22 +0000

    config server

--- Applications/RegistrationServer/Program.cs
+++ Applications/RegistrationServer/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Steeltoe.Extensions.Configuration.CloudFoundry;
+using Steeltoe.Extensions.Configuration.ConfigServer;
 using Steeltoe.Extensions.Logging;
 using Steeltoe.Common.Hosting;
 
@@ -16,7 +16,7 @@ namespace RegistrationServer
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .AddCloudFoundry()
+                 .AddConfigServer()
                 .UseCloudHosting()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
